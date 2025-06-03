558444e7a997ca7c87bacadaed91c3cbdba35f83
zhangfei <407136912@qq.com>
2025-06-03 16:19:16 +0800

    我的页面改造

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,181 +1,112 @@
 <template>
-  <qiun-loading v-if="isLoading" loadingType="2" />
-  <view
+  <SzLoading v-if="isLoading"></SzLoading>
+  <scroll-view
     v-else
-    class="viewport"
+    scroll-y
+    @scrolltolower="onScrollToLower"
+    class="h-screen viewport"
     :style="{ paddingTop: safeAreaInsets!.top + 'px' }">
     <!-- 添加背景图片 -->
-    <view class="user">
-      <view class="user-left">
-        <navigator url="/pagesMember/profile/profile" class="avatar-container">
+    <view
+      class="h-[300rpx] m-[30rpx_24rpx] p-[30rpx] rounded-[20rpx] flex items-center bg-white">
+      <view class="flex-none w-[150rpx]">
+        <navigator url="/pagesMember/profile/profile" class="block">
           <image
-            class="avatar"
+            class="w-[150rpx] h-[150rpx] rounded-[16rpx] border-[4rpx] border-white shadow-[0_2rpx_8rpx_rgba(0,0,0,0.1)]"
             src="https://picsum.photos/200/300?random=1"
             mode="aspectFill"></image>
         </navigator>
       </view>
-      <view class="user-middle">
-        <view class="user-name">大帅比</view>
-        <view class="user-detail">
+      <view class="flex-grow mx-[30rpx]">
+        <view class="text-[36rpx] font-semibold text-[#333] mb-[16rpx]">
+          大帅比
+        </view>
+        <view class="flex gap-[24rpx]">
           <!-- 关注，粉丝，获赞，拯救书荒 -->
-          <view class="user-info">
-            <view class="user-info-item">
-              <view class="user-info-label">关注</view>
-              <view class="user-info-value">0</view>
-            </view>
-            <view class="user-info-item">
-              <view class="user-info-label">粉丝</view>
-              <view class="user-info-value">0</view>
-            </view>
-            <view class="user-info-item">
-              <view class="user-info-label">获赞</view>
-              <view class="user-info-value">0</view>
-            </view>
-            <view class="user-info-item">
-              <view class="user-info-label">拯救书荒</view>
-              <view class="user-info-value">0</view>
-            </view>
+          <view class="flex flex-col items-center">
+            <view class="text-[24rpx] text-[#666] mb-[4rpx]">关注</view>
+            <view class="text-[30rpx] font-medium text-[#222]">0</view>
+          </view>
+          <view class="flex flex-col items-center">
+            <view class="text-[24rpx] text-[#666] mb-[4rpx]">粉丝</view>
+            <view class="text-[30rpx] font-medium text-[#222]">0</view>
+          </view>
+          <view class="flex flex-col items-center">
+            <view class="text-[24rpx] text-[#666] mb-[4rpx]">获赞</view>
+            <view class="text-[30rpx] font-medium text-[#222]">0</view>
+          </view>
+          <view class="flex flex-col items-center">
+            <view class="text-[24rpx] text-[#666] mb-[4rpx]">拯救书荒</view>
+            <view class="text-[30rpx] font-medium text-[#222]">0</view>
           </view>
         </view>
       </view>
-      <view class="user-right">
+      <view class="flex-none w-[40rpx] text-[#999] text-[32rpx]">
         <uni-icons type="right" size="30"></uni-icons>
       </view>
     </view>
 
     <!-- 工具栏目 -->
-    <view class="tool-bar">
+    <view
+      class="h-[300rpx] m-[24rpx] rounded-[16rpx] bg-white grid grid-cols-3 gap-2.5 p-[24rpx]">
       <!-- 浏览历史，阅读偏好，赞过的视频，反馈与在赞助，我的预约 -->
-      <view class="tool-bar-item">
+      <view
+        class="h-[96rpx] px-[36rpx] flex flex-col items-center justify-center text-[30rpx] text-[#222] whitespace-nowrap">
         <uni-icons type="fire" size="30"></uni-icons>
         <text>浏览历史</text>
       </view>
-      <view class="tool-bar-item">
+      <view
+        class="h-[96rpx] px-[36rpx] flex flex-col items-center justify-center text-[30rpx] text-[#222] whitespace-nowrap">
         <uni-icons type="map" size="30"></uni-icons>
         <text>阅读偏好</text>
       </view>
-      <view class="tool-bar-item">
+      <view
+        class="h-[96rpx] px-[36rpx] flex flex-col items-center justify-center text-[30rpx] text-[#222] whitespace-nowrap">
         <uni-icons type="videocam" size="30"></uni-icons>
         <text>赞过的视频</text>
       </view>
-      <view class="tool-bar-item">
+      <view
+        class="h-[96rpx] px-[36rpx] flex flex-col items-center justify-center text-[30rpx] text-[#222] whitespace-nowrap">
         <uni-icons type="info" size="30"></uni-icons>
         <text>反馈与赞助</text>
       </view>
-      <view class="tool-bar-item">
+      <view
+        class="h-[96rpx] px-[36rpx] flex flex-col items-center justify-center text-[30rpx] text-[#222] whitespace-nowrap">
         <uni-icons type="mail-open" size="30"></uni-icons>
         <text>我的预约</text>
       </view>
-      <view class="tool-bar-item">
+      <view
+        class="h-[96rpx] px-[36rpx] flex flex-col items-center justify-center text-[30rpx] text-[#222] whitespace-nowrap">
         <uni-icons type="gear" size="30"></uni-icons>
         <text>设置</text>
       </view>
     </view>
-  </view>
+
+    <!-- 2.推荐书籍模块 -->
+    <SzBookRecommend ref="bookRecommendRef" />
+  </scroll-view>
 </template>
 
 <script setup lang="ts">
+import { bookRecommendList } from '@/composables'
 import { onShow } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 const isLoading = ref(false)
-onShow(async () => {})
+onShow(async () => {
+  isLoading.value = true
+  setTimeout(() => {
+    isLoading.value = false
+  }, 1000)
+})
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
+// 猜你喜欢
+const { bookRecommendRef, onScrollToLower } = bookRecommendList()
 </script>
 
 <style lang="scss">
 .viewport {
-  min-height: 100vh;
-  background-color: #f8f8f8;
-  padding-bottom: 40rpx;
-
-  /* 用户信息区域 */
-  .user {
-    height: 300rpx;
-    margin: 30rpx 24rpx;
-    padding: 30rpx;
-    border-radius: 20rpx;
-    background: linear-gradient(135deg, #f6ece0 0%, #f8f1eb 50%, #f7f5e6 100%);
-    box-shadow: 0 4rpx 16rpx rgba(246, 236, 224, 0.4);
-    display: flex;
-    align-items: center;
-
-    .user-left {
-      flex: 0 0 150rpx;
-      .avatar-container {
-        .avatar {
-          width: 150rpx;
-          height: 150rpx;
-          border-radius: 16rpx;
-          border: 4rpx solid #fff;
-          box-shadow: 0 2rpx 8rpx rgba(0, 0, 0, 0.1);
-        }
-      }
-    }
-
-    .user-middle {
-      flex: 1;
-      margin: 0 30rpx;
-
-      .user-name {
-        font-size: 36rpx;
-        font-weight: 600;
-        color: #333;
-        margin-bottom: 16rpx;
-      }
-
-      .user-info {
-        display: flex;
-        gap: 24rpx;
-
-        .user-info-item {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-
-          .user-info-label {
-            font-size: 24rpx;
-            color: #666;
-            margin-bottom: 4rpx;
-          }
-
-          .user-info-value {
-            font-size: 30rpx;
-            font-weight: 500;
-            color: #222;
-          }
-        }
-      }
-    }
-
-    .user-right {
-      flex: 0 0 40rpx;
-      color: #999;
-      font-size: 32rpx;
-    }
-  }
-
-  /* 工具栏目 */
-  .tool-bar {
-    margin: 24rpx;
-    background: #f9f9f9;
-    border-radius: 16rpx;
-    box-shadow: 0 1rpx 8rpx rgba(0, 0, 0, 0.03);
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: 10px;
-
-    .tool-bar-item {
-      height: 96rpx;
-      padding: 0 36rpx;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      font-size: 30rpx;
-      color: #222;
-      white-space: nowrap;
-    }
-  }
+  // 渐变色背景色;
+  background: linear-gradient(to bottom, #edcea7, #cfed8b, #4c90e9);
 }
 </style>
