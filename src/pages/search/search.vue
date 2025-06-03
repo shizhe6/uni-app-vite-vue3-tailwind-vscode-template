078c2a2d8e9dfd500910b78bc565fc8dd477992a
zhangfei <407136912@qq.com>
2025-06-03 17:51:15 +0800

    搜索页面样式修改

--- src/pages/search/search.vue
+++ src/pages/search/search.vue
@@ -1,60 +1,74 @@
 <template>
-  <view class="search-container">
+  <view class="flex flex-col h-screen">
     <!-- 搜索框 -->
-    <view class="top-bar">
-      <view class="top-bar-back" @click="handleBack">
-        <uni-icons type="left" size="24" color="#333" />
-      </view>
+    <view
+      class="h-[40px] flex items-end justify-center fixed w-full z-1000 bg-white px-4">
       <uni-easyinput
         prefixIcon="search"
         placeholder="请输入搜索关键词"
         v-model="keyword"
-        @confirm="handleSearch"
-      ></uni-easyinput>
-      <text class="top-bar-text">搜索</text>
+        @confirm="handleSearch"></uni-easyinput>
     </view>
 
     <!-- 滚动内容 -->
-    <scroll-view class="middle-content" scroll-y>
+    <scroll-view
+      class="flex-1 flex flex-col mt-[40px] overflow-hidden"
+      scroll-y>
       <!-- 搜索历史 -->
-      <view class="search-history">
-        <view class="search-history-header">
-          <text class="title">搜索历史</text>
-          <uni-icons type="trash" size="18" @click="clearHistory" />
+      <view class="h-auto mb-4 my-4">
+        <view class="flex justify-between items-center px-8">
+          <text class="title text-7 font-bold">搜索历史</text>
+          <view @click="toggleShowAll = !toggleShowAll">
+            <text>{{ toggleShowAll ? '收起 ˄' : '展开 ˅' }}</text>
+          </view>
         </view>
-        <view class="search-history-tag-list">
+        <!-- 历史列表 -->
+        <view class="flex flex-wrap gap-5 p-5">
           <uni-tag
-            v-for="(item, index) in searchHistory"
+            v-for="(item, index) in toggleShowAll
+              ? searchHistory
+              : searchHistory.slice(0, 12)"
             :key="index"
             :text="item"
             :inverted="true"
-            @click="searchFromHistory(item)"
-          />
+            @click="searchFromHistory(item)" />
         </view>
       </view>
 
       <!-- 热搜榜单 -->
-      <view class="hot-search">
-        <scroll-view scroll-x class="hot-search-scroll">
-          <view class="hot-list-container">
-            <view v-for="(list, index) in hotSearchLists" :key="index" class="hot-item-list">
+      <view class="flex-1 overflow-y-auto">
+        <scroll-view scroll-x class="w-full whitespace-nowrap">
+          <view class="inline-flex">
+            <view
+              v-for="(list, index) in hotSearchLists"
+              :key="index"
+              class="w-52 m-4 p-4 rank-backgroud-color rounded-lg">
               <!-- 推荐榜单名称 -->
-              <view class="list-header">
-                <text class="title">{{ list.title }}</text>
+              <view class="flex items-center mb-5">
+                <text class="font-bold pl-2.5 mr-2.5">
+                  {{ list.title }}
+                </text>
                 <uni-icons type="right" size="14" color="#999" />
               </view>
               <!-- 推荐榜单内容 -->
-              <view class="list-content">
+              <view class="flex flex-col gap-5">
                 <view
                   v-for="(item, idx) in list.items"
                   :key="idx"
-                  class="book-item"
-                  @click="handleHotSearch(item)"
-                >
-                  <text class="book-index" :class="{ top3: idx < 3 }">{{ idx + 1 }}</text>
-                  <view class="book-content">
-                    <text class="book-name">{{ item.name }}</text>
-                    <text class="book-heat">{{ item.heat }}</text>
+                  class="flex flex-row items-center py-2.5"
+                  @click="handleHotSearch(item)">
+                  <!-- 排名 -->
+                  <text class="w-10">
+                    {{ idx + 1 }}
+                  </text>
+                  <!-- 标题 -->
+                  <view class="flex flex-column">
+                    <text>
+                      {{ item.name }}
+                    </text>
+                    <text>
+                      {{ item.heat }}
+                    </text>
                   </view>
                 </view>
               </view>
@@ -69,6 +83,9 @@
 <script lang="ts" setup>
 import { onShow } from '@dcloudio/uni-app'
 import { ref } from 'vue'
+
+// toggleShowAll
+const toggleShowAll = ref(false)
 
 const keyword = ref('')
 const searchHistory = ref([
@@ -110,7 +127,7 @@ const searchHistory = ref([
   '都市言情',
   '科幻未来',
   '玄幻小说',
-  '都市言情',
+  '都市言情'
 ])
 const hotSearchLists = ref([
   {
@@ -125,8 +142,8 @@ const hotSearchLists = ref([
       { order: 7, name: '影帝的隐婚娇妻', heat: '142万' },
       { order: 8, name: '穿书后我成团宠', heat: '135万' },
       { order: 9, name: '学霸的科技帝国', heat: '128万' },
-      { order: 10, name: '风水相师在都市', heat: '118万' },
-    ],
+      { order: 10, name: '风水相师在都市', heat: '118万' }
+    ]
   },
   {
     title: '热搜短剧榜',
@@ -140,8 +157,8 @@ const hotSearchLists = ref([
       { order: 7, name: '将军夫人要翻身', heat: '129万' },
       { order: 8, name: '腹黑世子俏医妃', heat: '122万' },
       { order: 9, name: '冷王盛宠医妃狂', heat: '115万' },
-      { order: 10, name: '穿书后我成了反派', heat: '108万' },
-    ],
+      { order: 10, name: '穿书后我成了反派', heat: '108万' }
+    ]
   },
   {
     title: '热搜漫画榜',
@@ -155,10 +172,12 @@ const hotSearchLists = ref([
       { order: 7, name: '斗罗大陆', heat: '221万' },
       { order: 8, name: '火凤燎原', heat: '208万' },
       { order: 9, name: '天行九歌', heat: '195万' },
-      { order: 10, name: '秦时明月', heat: '182万' },
-    ],
-  },
+      { order: 10, name: '秦时明月', heat: '182万' }
+    ]
+  }
 ])
+
+// showHistoryList
 
 const handleSearch = () => {
   if (!keyword.value) return
@@ -183,146 +202,12 @@ const { safeAreaInsets } = uni.getSystemInfoSync()
 
 //设置标题为空
 onShow(() => {})
-
-// 添加返回方法
-const handleBack = () => {
-  uni.navigateBack()
-}
 </script>
 
 <style lang="scss">
-::-webkit-scrollbar {
-  display: none;
-  width: 0 !important;
-  height: 0 !important;
-  -webkit-appearance: none;
-  background: transparent;
-}
-.search-container {
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
+.rank-backgroud-color {
+  background-color: #ebb6b6;
 
-  // 头部搜索框
-  .top-bar {
-    height: 90px;
-    display: flex;
-    align-items: flex-end;
-    justify-content: center;
-    position: fixed;
-    width: 100%;
-    z-index: 1000;
-    background-color: #fff;
-    .top-bar-back {
-    }
-    .top-bar-text {
-    }
-  }
-
-  // 内容区域
-  .middle-content {
-    flex: 1;
-    flex-direction: column;
-    padding: 20px;
-    padding-top: calc(100px + env(safe-area-inset-top));
-    height: calc(100vh - 100px - env(safe-area-inset-top));
-    .search-history {
-      height: auto;
-      margin-bottom: 40px;
-      .search-history-header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-bottom: 20rpx;
-
-        .title {
-          font-size: 28rpx;
-          font-weight: bold;
-        }
-      }
-      .search-history-tag-list {
-        display: flex;
-        flex-wrap: wrap;
-        gap: 20rpx;
-      }
-    }
-    .hot-search {
-      flex: 1;
-      overflow-y: auto;
-      .hot-search-scroll {
-        width: 100%;
-        white-space: nowrap;
-
-        .hot-list-container {
-          display: inline-flex;
-          gap: 30rpx;
-          padding: 0 20rpx;
-
-          .hot-item-list {
-            width: 320rpx;
-            background: #fff8f6;
-            border-radius: 16rpx;
-            padding: 20rpx;
-            box-shadow: 0 4rpx 12rpx rgba(0, 0, 0, 0.1);
-
-            .list-header {
-              display: flex;
-              align-items: center;
-              margin-bottom: 20rpx;
-
-              .title {
-                font-size: 26rpx;
-                font-weight: bold;
-                padding-left: 10rpx;
-                border-left: 6rpx solid #d6a364;
-                margin-right: 10rpx;
-              }
-            }
-
-            .list-content {
-              display: flex;
-              flex-direction: column;
-              gap: 20rpx;
-
-              .book-item {
-                display: flex;
-                flex-direction: row;
-                align-items: center;
-                padding: 10rpx 0;
-
-                .book-index {
-                  width: 40rpx;
-                  margin-right: 15rpx;
-                  color: #999;
-                  font-size: 28rpx;
-
-                  &.top3 {
-                    color: #d6a364;
-                    font-weight: bold;
-                  }
-                }
-
-                .book-content {
-                  display: flex;
-                  flex-direction: column;
-                  .book-name {
-                    flex: 1;
-                    overflow: hidden;
-                    text-overflow: ellipsis;
-                    font-size: 28rpx;
-                  }
-
-                  .book-heat {
-                    color: #999;
-                    font-size: 24rpx;
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+  border: 1px solid #e61d1d;
 }
 </style>
