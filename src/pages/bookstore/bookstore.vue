72d707e175ddb816faf041c235eafe6e1502f0d5
shizhe6 <407136912@qq.com>
2025-06-10 22:58:38 +0800

    1.第一节：底部tabBar页面新增和切换特效

---
+++ src/pages/bookstore/bookstore.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <view class="viewreport">
+    书城
+  </view>
+</template>
+
+<style  lang="scss" scoped>
+.viewreport {
+  background-color: antiquewhite;
+}
+</style>
