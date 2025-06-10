d30de4857f6b7b9b049884e0f5af0904ec305178
zhangfei <407136912@qq.com>
2025-06-10 14:52:18 +0800

    1

--- src/pages/bookstore/components/KnowledgeContent.vue
+++ src/pages/bookstore/components/KnowledgeContent.vue
@@ -1,13 +1,74 @@
 <template>
-  <view class="content">
-    <text class="title">知识分类内容</text>
-  </view>
+  <SzLoading v-if="isLoading"></SzLoading>
+  <scroll-view v-else scroll-y @scrolltolower="onScrollToLower">
+    <!-- 2.推荐书籍模块 -->
+    <!-- 根据错误提示，需要补充 sourceType 属性，这里假设 sourceType 的值为 'recommend'，可根据实际情况修改 -->
+    <SzBookRecommend ref="bookRecommendRef" sourceType="知识" />
+  </scroll-view>
 </template>
 
-<style scoped>
-.title {
-  font-size: 32rpx;
-  color: #333;
-  padding: 30rpx;
+<script setup lang="ts">
+import { bookRecommendList } from '@/composables'
+import { onMounted } from 'vue'
+
+// 猜你喜欢
+const { bookRecommendRef, onScrollToLower } = bookRecommendList()
+
+// 数据加载方法
+const loadData = async () => {
+  console.log('知识页面加载数据')
+}
+
+// 页面加载状态
+const isLoading = ref(false)
+// 触发加载：页面显示或组件挂载时
+onMounted(() => {
+  //开启数据加载状态
+  isLoading.value = true
+  // 加载数据
+  loadData()
+
+  // 关闭数据加载状态
+  setTimeout(() => {
+    isLoading.value = false
+  }, 1000)
+})
+</script>
+
+<style lang="scss" scoped>
+.classic-item {
+  padding: 20rpx;
+  margin: 20rpx;
+  background: #fff;
+  border-radius: 16rpx;
+  box-shadow: 0 4rpx 12rpx rgba(0, 0, 0, 0.08);
+  display: flex;
+
+  .cover-image {
+    width: 200rpx;
+    height: 280rpx;
+    border-radius: 8rpx;
+    margin-right: 20rpx;
+  }
+
+  .content-info {
+    flex: 1;
+    .title {
+      font-size: 32rpx;
+      font-weight: bold;
+      margin-bottom: 10rpx;
+    }
+    .author {
+      font-size: 26rpx;
+      color: #666;
+    }
+    .meta {
+      margin-top: 15rpx;
+      font-size: 24rpx;
+      color: #999;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
 }
 </style>
